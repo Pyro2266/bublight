3d4622c4fbe8c557d7ffdeb4c81ce450c1f6f326
Samuel Kontriš <pyro2266@gmail.com>
2018-05-21 21:37:28 +0200

    First PoC of changing LED intensity by difference in pressure

--- src/main/java/com/github/pyro2266/lightit/led/modes/SimpleColorMode.java
+++ src/main/java/com/github/pyro2266/lightit/led/modes/SimpleColorMode.java
@@ -1,10 +1,15 @@
 package com.github.pyro2266.lightit.led.modes;
 
 import com.github.mbelling.ws281x.Color;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SimpleColorMode implements LedMode {
 
+    private static final Logger LOG = LoggerFactory.getLogger(SimpleColorMode.class);
     private static final String MODE_ID = "Simple";
+
+    private Color[] colors;
 
     @Override
     public String getModeId() {
@@ -13,6 +18,19 @@ public class SimpleColorMode implements LedMode {
 
     @Override
     public Color[] getNextColors(Color[] previousColors) {
-        return new Color[0];
+        if (colors != null) {
+            return colors;
+        } else {
+            LOG.warn("Colors are not set!");
+            return previousColors;
+        }
+    }
+
+    public Color[] getColors() {
+        return colors;
+    }
+
+    public void setColors(Color[] colors) {
+        this.colors = colors;
     }
 }
