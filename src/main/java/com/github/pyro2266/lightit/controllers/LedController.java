c2b52c415ecefe051ee0dbedadd313f920922007
Samuel Kontriš <pyro2266@gmail.com>
2018-05-29 18:58:33 +0200

    Fixed merge fail and added controller for mode switching

--- src/main/java/com/github/pyro2266/lightit/controllers/LedController.java
+++ src/main/java/com/github/pyro2266/lightit/controllers/LedController.java
@@ -1,20 +1,14 @@
 package com.github.pyro2266.lightit.controllers;
 
 import com.github.pyro2266.lightit.led.core.LedService;
-import com.github.pyro2266.lightit.pressure.PressureException;
-import com.github.pyro2266.lightit.pressure.PressureService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-// TODO Refactor this class to reflect changes in modes. Probably create ModeController class and move API for
-// switching and configuring modes there.
 
 @RestController
 @RequestMapping(path = "/led")
@@ -25,44 +19,24 @@ public class LedController {
     @Autowired
     private LedService ledService;
 
-    /*
-    @Autowired
-    private PressureService pressureService;
-
     @PostMapping(path = "/start")
     public ResponseEntity start() {
-        try {
-            // TODO move
-            pressureService.calibrateNormalPressure();
-        } catch (PressureException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+        LOG.info("Request to start color loop received");
         ledService.startColorLoop();
         return ResponseEntity.ok().build();
     }
 
     @PostMapping(path = "/stop")
     public ResponseEntity stop() {
+        LOG.info("Request to stop color loop received");
         ledService.stopColorLoop();
         return ResponseEntity.ok().build();
     }
 
-
     @PostMapping(path = "/setRefreshRate/{rate}")
     public ResponseEntity setRefreshRate(@PathVariable int rate) {
+        LOG.info("Request to change refresh rate to {} received", rate);
         ledService.setRefreshRate(rate);
         return ResponseEntity.ok().build();
     }
-
-    @PostMapping(path = "/setRange/{range}")
-    public ResponseEntity setRange(@PathVariable int range) {
-        ledService.setPressureRange(range);
-        return ResponseEntity.ok().build();
-    }
-
-    @PostMapping(path = "/setDefaultBrightness/{brightness}")
-    public ResponseEntity setDefaultBrightness(@PathVariable int brightness) {
-        ledService.setDefaultBrightness(brightness);
-        return ResponseEntity.ok().build();
-    }*/
 }
---
+++ src/main/java/com/github/pyro2266/lightit/controllers/LedModeController.java
@@ -0,0 +1,74 @@
+package com.github.pyro2266.lightit.controllers;
+
+import com.github.pyro2266.lightit.led.core.LedService;
+import com.github.pyro2266.lightit.led.modes.impl.base.SimpleColorMode;
+import com.github.pyro2266.lightit.led.modes.impl.base.SimpleColorModeConfig;
+import com.github.pyro2266.lightit.led.modes.impl.base.SimpleRainbowMode;
+import com.github.pyro2266.lightit.led.modes.impl.overlay.BrightnessByPressureMode;
+import com.github.pyro2266.lightit.led.modes.impl.overlay.BrightnessByPressureModeConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/mode")
+public class LedModeController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LedModeController.class);
+
+    @Autowired
+    private LedService ledService;
+
+    @Autowired
+    private SimpleColorMode simpleColorMode;
+
+    @Autowired
+    private SimpleRainbowMode simpleRainbowMode;
+
+    @Autowired
+    private BrightnessByPressureMode brightnessByPressureMode;
+
+    @PostMapping(path = "/base/simpleColorMode")
+    public ResponseEntity activateSimpleColorMode(@RequestBody SimpleColorModeConfig config) {
+        LOG.info("Activating {} with config {}", SimpleColorMode.class, config);
+        simpleColorMode.setConfig(config);
+        ledService.setBaseLedMode(simpleColorMode);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(path = "/base/simpleColorMode")
+    public ResponseEntity getCurrentSimpleColorModeConfig() {
+        SimpleColorModeConfig config = simpleColorMode.getConfig();
+        LOG.info("Current config for {} is {}", SimpleColorMode.class, config);
+        return ResponseEntity.ok(config);
+    }
+
+    @PostMapping(path = "/base/simpleRainbowMode")
+    public ResponseEntity activateSimpleRainbowMode() {
+        LOG.info("Activating {}", SimpleRainbowMode.class);
+        ledService.setBaseLedMode(simpleRainbowMode);
+        return ResponseEntity.ok().build();
+
+    }
+
+    @PostMapping(path = "/overlay/brightnessByPressureMode")
+    public ResponseEntity activateBrightnessByPressureMode(@RequestBody BrightnessByPressureModeConfig config) {
+        LOG.info("Activating {} with config {}", BrightnessByPressureMode.class, config);
+        brightnessByPressureMode.setConfig(config);
+        ledService.setOverlayLedMode(brightnessByPressureMode);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(path = "/overlay/brightnessByPressureMode")
+    public ResponseEntity getCurrentBrightnessByPressureModeConfig() {
+        BrightnessByPressureModeConfig config = brightnessByPressureMode.getConfig();
+        LOG.info("Current config for {} is {}", BrightnessByPressureMode.class, config);
+        return ResponseEntity.ok(config);
+    }
+}
