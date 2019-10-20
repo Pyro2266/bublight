ed72242f4af430f966d935cb13c0bd019a98ce9f
Samuel Kontriš <pyro2266@gmail.com>
2019-10-20 21:33:49 +0200

    Implement websocket notifier when pressure difference changes

--- lightit-backend/src/main/java/com/github/pyro2266/lightit/websocket/PressureWebsocketService.java
+++ lightit-backend/src/main/java/com/github/pyro2266/lightit/websocket/PressureWebsocketService.java
@@ -1,12 +1,27 @@
 package com.github.pyro2266.lightit.websocket;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PressureWebsocketService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(PressureWebsocketService.class);
+
+    public final String PRESSURE_DIFF_WEBSOCKET_ENDPOINT = "/pressureSubscribe";
+
+    private final SimpMessagingTemplate template;
+
+    public PressureWebsocketService(SimpMessagingTemplate template) {
+        this.template = template;
+    }
+
     public void onPressureDiffChange(float pressureDiff) {
-        // TODO send new pressureDiff to all subscribers
+        LOG.trace("Sending new pressure {} to websocket subscribers at {}", pressureDiff,
+                PRESSURE_DIFF_WEBSOCKET_ENDPOINT);
+        this.template.convertAndSend(PRESSURE_DIFF_WEBSOCKET_ENDPOINT, pressureDiff);
     }
 
 }
