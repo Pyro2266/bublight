43250c4876b39805291b4aa233fcb383e7ce219d
Marek Gajdoš <marek.frnzo@gmail.com>
2021-03-22 12:15:42 +0100

    Remove example component | remove useless function

--- bublight-webapp/src/main/vue/bublight-webapp/src/components/panel-mode/mode-panel.component.vue
+++ bublight-webapp/src/main/vue/bublight-webapp/src/components/panel-mode/mode-panel.component.vue
@@ -82,15 +82,6 @@
         },
 
         methods: {
-            hexToRgb(hex) {
-                hex = hex.replace('#','')
-                var r = parseInt(hex.substring(0,2), 16)
-                var g = parseInt(hex.substring(2,4), 16)
-                var b = parseInt(hex.substring(4,6), 16)
-
-                return [r, g, b]
-            },
-
             modeSwitch(mode) {
                 switch(mode) {
                     case 0:
